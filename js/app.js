17062bd56e9c4750c5f37ba7ed572807879d641d
navy <jezreelsanz@gmail.com>
2018-07-30 13:10:05 -0500

    se hacen cambios en nombres de algunas funciones y estilos

--- js/app.js
+++ js/app.js
@@ -9,10 +9,31 @@ const admitDrop = (e) => {
 
 const drop = (e) => {
     e.preventDefault();
-    let id_foto = e.dataTransfer.getData('text');
-    e.target.appendChild(document.getElementById(id_foto));
+    let data = e.dataTransfer.getData('text');
+    e.target.appendChild(document.getElementById(data));
 }
 
 const dragEnd = (e) => {
     e.target.style.opacity = "1";
 }
+
+
+
+/*document.addEventListener("dragstart", function(event) {
+    event.dataTransfer.setData("Text", event.target.id);
+    event.target.style.opacity = "0.4";
+});
+
+document.addEventListener("drop", function(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("Text");
+    event.target.appendChild(document.getElementById(data));
+});
+
+document.addEventListener("dragover", function(event) {
+    event.preventDefault();
+});
+
+document.addEventListener("dragend", function(event) {
+    event.target.style.opacity = "1";
+});*/
